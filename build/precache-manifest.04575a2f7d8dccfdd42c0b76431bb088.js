0232b33cc84f8f0cd115332417ec9e2f55c4be2d
DELL <ehteasham@gmail.com>
2020-04-12 00:32:55 +0500

    build added

---
+++ build/precache-manifest.04575a2f7d8dccfdd42c0b76431bb088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fada922f92e195a969c8792369ee894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869ce68bf2dfcab58c0d",
+    "url": "/static/css/main.3f815c63.chunk.css"
+  },
+  {
+    "revision": "5a3ee3f6f4974071d341",
+    "url": "/static/js/2.cc9e8249.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/static/js/2.cc9e8249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869ce68bf2dfcab58c0d",
+    "url": "/static/js/main.c77288a7.chunk.js"
+  },
+  {
+    "revision": "f6f18e47675acceed997",
+    "url": "/static/js/runtime-main.f93ae6d3.js"
+  }
+]);
