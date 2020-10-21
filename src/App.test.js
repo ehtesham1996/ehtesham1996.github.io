f20338462f0b10a0d86f494eb1f27ec2ece8feed
ehtesham1996 <ehteasham@gmail.com>
2020-10-21 19:57:36 +0500

    github workflows added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Testing app', () => {
+  test('should Run properly', () => {
+    console.log("Sample test case run");
+  })
+
 });
