dc6bf0fa3ce808c79dd0b9c92d30041a5ab4fa9d
ehtesham1996 <ehteasham@gmail.com>
2020-10-22 14:43:19 +0500

    workflow and eslint bugs fixed

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -34,6 +34,10 @@ export default class Testimonials extends Component {
                       return {
                         "display": "None"
                       };
+                    default:
+                      return {
+                        "display": "None"
+                      };
                   }
                 }}
 
